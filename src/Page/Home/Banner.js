0bf4c0ba224e8e3785ebb6af50eaa445a7be6417
capmahin <mahinulhuque1996@gmail.com>
2022-09-28 05:57:56 +0600

    finish

--- src/Page/Home/Banner.js
+++ src/Page/Home/Banner.js
@@ -2,52 +2,63 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="https://static.tweaktown.com/news/8/6/86262_02_asus-rog-strix-scar-17-se-gaming-laptop-core-i9-12950hx-rtx-3080-ti_full.jpg"
-          className="w-96	"
-        />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
+    <div>
+      <div className="carousel w-screen">
+        <div id="slide1" className="carousel-item relative w-screen">
+          <img
+            src="https://www.techadvisor.com/wp-content/uploads/2022/06/best_gaming_laptop.jpg?quality=50&strip=all"
+            className="w-screen h-96"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src="https://placeimg.com/800/200/arch" className="w-96" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
+        <div id="slide2" className="carousel-item relative w-screen">
+          <img
+            src="https://assets-prd.ignimgs.com/2022/04/20/alienwarelaptop-1650471128761.jpg?fit=bounds&width=1280&height=720"
+            className="w-screen h-96"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src="https://placeimg.com/800/200/arch" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
+        <div id="slide3" className="carousel-item relative w-screen">
+          <img
+            src="https://dlcdnwebimgs.asus.com/gain/614D9088-45C8-46DE-88EB-D963616A5548/fwebp"
+            className="w-screen h-96"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src="https://placeimg.com/800/200/arch" className="w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
-          </a>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://techuda.com/wp-content/uploads/2022/01/asus_laptops_2022_asus_zephyrus_duo_16-1024x538.jpg"
+            className="w-screen h-96"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
       </div>
     </div>
