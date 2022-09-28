6d3daee3d36da681e1c6c78a4411795244e18a7e
capmahin <mahinulhuque1996@gmail.com>
2022-09-29 02:27:52 +0600

    solve team problem

--- src/Page/Home/Team.js
+++ src/Page/Home/Team.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 const Team = ({ team }) => {
-  const { name, img, description } = team;
+  const { name, img, description, description1 } = team;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
         <img src={img} alt="Parts" className="rounded-xl" />
       </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+      <div className="card-body items-right text-center">
+        <h2 className="card-title text-2xl">{name}</h2>
+        <p className="text-left text-xl text-primary">{description}</p>
+        <p className="text-left  ">{description1}</p>
       </div>
     </div>
   );
