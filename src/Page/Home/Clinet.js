8cc927ec52596cf918665978c141403f33b54bd7
capmahin <mahinulhuque1996@gmail.com>
2022-09-29 02:21:10 +0600

    solve client problem

--- src/Page/Home/Clinet.js
+++ src/Page/Home/Clinet.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Clinet = ({ clinet }) => {
-  const { name, img, description } = clinet;
+  const { name, img, description, comment } = clinet;
   return (
     <div className="card w-80 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
         <img src={img} alt="Parts" className="rounded-xl" />
       </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+      <div className="card-body items-right text-center">
+        <h2 className="card-title text-3xl">{name}</h2>
+        <p className="text-left text-2xl text-green-600">{description}</p>
+        <h1 className="text-left text-xl text-primary">"Comment":</h1>
+        <p className="text-justify ">{comment}</p>
       </div>
     </div>
   );
